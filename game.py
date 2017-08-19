8718045d8970febcd31e89477a57e3c779f68f6f
Marijn De Kerpel <marijn.dekerpel@gmail.com>
2017-08-20 01:37:55 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,18 +1,25 @@
 import csv
 
-
 class Pot(object):
-	def __init__(self, trump):
-		self.trump = trump
+	"""pot object that keeps track of an ongoing round"""
+
+	def __init__(self):
+		self.trump = None
 		self.strike = []
-		self.winner = None
+		self.last_winner = None
+		self.last_strike = []
 
 	def __str__(self):
+		
 		return str(self.strike)
-
 	def __add__(self, other):
 		if len(self.strike) >= 4:
-			self.winner = None
+			#figuring out who won
+			self.last_winner = self.round_winner()
+
+			#end of a strike
+			self.last_strike = self.strike
+			self.strike = []
 
 		else:
 			self.strike.append(other)
@@ -20,21 +27,52 @@ class Pot(object):
 		return self
 		
 
-class GameHistory(object):
+	def round_winner(self):
+		"""returns the winner of the round"""
+		
+		trump = self.trump
+
+		#is there a trump played?
+		contenders = [card for card in self.strike if card.color == trump ]
+		
+		#there is no trump played
+		if len(contenders) == 0:
+			trump = self.strike[0].color
+			contenders = [card for card in self.strike  if card.color == trump]
+			return highest_value_card(contenders).played_by
+		
+		#there is a trump played
+		else:
+			return highest_value_card(contenders).played_by
+
+
+
+def highest_value_card(contenders):
+	
+	contenders_values = [contender.value for contender in contenders]
+	return contenders[contenders_values.index(max(contenders_values))]
+
+
+
+class GameLog(object):
+	"""objects used to log the game and print it to a .csv file"""
+
 	def __init__(self, bestandsnaam):
 		self.bestandsnaam = bestandsnaam
 		self.history = []
 
 	def update(self, pot):
 		if len(self.history >= 8):
-			print(pot)
+			#end of a game
+			with open(bestandsnaam, 'w', newline='') as gamelog_file:
+				writer =  gamelog_file.writer(gamelog_file)
+				
+				for round_log in self.history:
+					writer.writerow(round_log)
 
-		else
-			self.history.append(pot)
+			print(self.history)
 
-		return self
+		else:
+			self.history.append(pot.last_strike)
 
-	def next_round(self):
-		self.old_strikes.append(list(self.strike))
-		self.strike = []
 		return self
